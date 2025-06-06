7bcd8b9bccd2bd6cbe9417b91a861a87b6bf87d0
Wilker Martins <dev.wilker@gmail.com>
2025-06-05 21:00:39 -0300

    ajuste link

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import AppointmentForm from "@/components/AppointmentForm/AppointmentForm"; // Import the form component
 
